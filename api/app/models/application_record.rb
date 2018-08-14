50ba19fb3109fd2e62c4de752b494ff73c3233e1
GuttermanA <alexanderf.gutterman@gmail.com>
2018-08-14 00:49:53 -0400

    wrote more SQL

--- api/app/models/application_record.rb
+++ api/app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.convert_ms_to_datetime(ms)
+    Time.at(ms/1000)
+  end
 end
--- api/app/models/log.rb
+++ api/app/models/log.rb
@@ -53,57 +53,113 @@ class Log < ApplicationRecord
     Log.find_by_sql [sql]
   end
 
-  def self.first_status(component_name)
+  def self.first_status(component_name = "COMPONENT000000001")
     sql = <<-SQL
       SELECT DISTINCT
+        logs.id,
         logs.component_name,
         logs.component_type,
         logs.location,
         logs.status,
-        logs.change_ts
+        MIN(logs.change_ts) min_change_ts
       FROM logs
-      INNER JOIN (
-        SELECT logs.component_name, MIN(logs.change_ts) AS min_change_ts FROM logs WHERE logs.component_name = ? GROUP BY logs.component_name
-      )min
-      ON logs.component_name = min.component_name
-      AND logs.change_ts = min.min_change_ts
       WHERE
         logs.component_name = ?
+      GROUP BY logs.component_name
     SQL
 
-    Log.find_by_sql [sql, component_name, component_name]
+    Log.find_by_sql [sql, component_name]
   end
 
-  def self.last_status(component_name)
+  def self.last_status(component_name = "COMPONENT000000001")
     sql = <<-SQL
       SELECT DISTINCT
-        logs.*
-      FROM logs
-      INNER JOIN (
-        SELECT logs.component_name, MAX(logs.change_ts) AS max_change_ts FROM logs WHERE logs.component_name = ? GROUP BY logs.component_name
-      )max
-      ON logs.component_name = max.component_name
-      AND logs.change_ts = max.max_change_ts
-      WHERE
-        logs.component_name = ?
-    SQL
-
-    Log.find_by_sql [sql, component_name, component_name]
-  end
-
-  def self.mean_completition_time_by_component_type
-    sql = <<-SQL
-      SELECT DISTINCT
+        logs.id,
+        logs.component_name,
         logs.component_type,
-
-
+        logs.location,
+        logs.status,
+        MAX(logs.change_ts) max_change_ts
+      FROM logs
+      WHERE
+        logs.component_name = ?
+      GROUP BY logs.component_name
     SQL
 
+    Log.find_by_sql [sql, component_name]
   end
 
-  def self.convert_ms_to_datetime(ms)
-    Time.at(ms/1000)
+  def self.minutes_to_completion(component_name = "COMPONENT000000001")
+    sql = <<-SQL
+      SELECT DISTINCT
+        logs.id,
+        logs.component_name,
+        logs.component_type,
+        MIN(logs.change_ts) AS min_change_ts,
+        MAX(logs.change_ts) AS max_change_ts,
+        (JULIANDAY(MAX(logs.change_ts)) - JULIANDAY(MIN(logs.change_ts))) * 24 * 60 AS minutes_to_completion
+      FROM logs
+      WHERE
+        logs.component_name = ?
+      GROUP BY logs.component_name
+    SQL
+
+    Log.find_by_sql [sql, component_name]
   end
+
+  def self.mean_completion_time_by_component_type
+    sql = <<-SQL
+      DROP TABLE IF EXISTS temp;
+
+      CREATE TEMPORARY TABLE temp AS
+      SELECT
+        logs.component_name,
+        logs.component_type,
+        (JULIANDAY(MAX(logs.change_ts)) - JULIANDAY(MIN(logs.change_ts))) * 24 * 60 AS minutes_to_completion
+      FROM logs
+      GROUP BY logs.component_name
+      HAVING minutes_to_completion > 0
+
+      SELECT
+    		component_type,
+    		AVG(minutes_to_completion) AS avg_completion_minutes
+  		FROM temp
+  		GROUP BY component_type;
+    SQL
+  end
+
+  def self.mean_component_completion_time_by_location
+
+
+  end
+
+  def rate_of_output_per_component_type(format = "min")
+    divisor = 1
+    if format == "sec"
+    else if format == "min"
+      divisor =
+    else if format == "hour"
+      divisor = 24
+    end
+
+
+    start_date = '2018-01-01 00:00:00'
+    end_date = '2018-06-01 00:00:20'
+    date_diff = (Date.parse('2018-06-01 00:00:20') - Date.parse('2018-01-01 00:00:00')).to_f
+    sql = <<-SQL
+
+      SELECT
+        logs.component_type,
+        COUNT(logs.component_name) AS total_components,
+        (COUNT(logs.component_type) / ?)
+
+
+      FROM logs
+      WHERE status = 'complete'
+
+  end
+
+
 
 
 
